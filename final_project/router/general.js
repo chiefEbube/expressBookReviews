b15bf636f2cc2cf2319b7480c48e03256218ae11
chiefebube <ebubechi.anyanwu@yahoo.com>
2025-11-06 09:37:07 -0500

    getting the book details based on the author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,35 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
     res.send(JSON.stringify(books,null,4))
-  return res.status(200).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn])
+
+    if (books[isbn]){
+        res.send(books[isbn]);
+    } else {
+        res.send("Book not available");
+    }
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const requestedAuthor = req.params.author
+    const booksKeys = Object.keys(books);
+    let matches = []
+
+  booksKeys.forEach((key) => {
+    if (books[key].author.toLowerCase() === requestedAuthor.toLowerCase()){
+        matches.push(books[key]);
+    }});
+
+    if (matches.length > 0) {
+        res.send(matches)
+    } else {
+        res.status(404).send("Author not found")
+    }
 });
 
 // Get all books based on title
