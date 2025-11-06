20c77b9398a92c87e76077a9cd95aa516c64e2cb
chiefebube <ebubechi.anyanwu@yahoo.com>
2025-11-06 10:51:16 -0500

    task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -26,7 +26,18 @@ regd_users.post("/login", (req,res) => {
   const {username, password} = req.body;
 
   if (!username || !password) {
-    return res.status(404).json({message: "e"})
+    return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+        data: password
+    }, 'access', { expiresIn: 60 * 60});
+
+    req.session.authorization = {accessToken, username}
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid credentials"});
   }
 });
 
